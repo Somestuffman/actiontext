3d22a90675d6852c1585586663e7eefbafb484d3
Somestuffman <semen1232007@yandex.ru>
2018-10-30 23:41:27 +0300

    rollback

--- lib/action_text/engine.rb
+++ lib/action_text/engine.rb
@@ -28,5 +28,18 @@ module ActionText
         helper ActionText::Engine.helpers
       end
     end
+
+    initializer "action_text.config" do
+      config.after_initialize do |app|
+        ActionText.renderer ||= ActionController::Base.renderer
+
+        # FIXME: ApplicationController should have a per-request specific renderer
+        # that's been set with the request.env env, and ActionText should just piggyback off
+        # that by default rather than doing this work directly.
+        ActionController::Base.before_action do
+          ActionText.renderer = ActionText.renderer.new(request.env)
+        end
+      end
+    end
   end
 end
